fe205c697a0289661892601cefe402ee5152813d
kevinbomb <kevinwomb@gmail.com>
2022-09-08 13:46:18 +0700

    GD3_C_0547

---
+++ app/src/main/java/com/example/gd3_c_0547/entity/Mahasiswa.kt
@@ -0,0 +1,17 @@
+package com.example.gd3_c_0547.entity
+
+class Mahasiswa(var name: String, var IPK: Double, var tahunMasuk: Int) {
+
+    companion object{
+        @JvmField
+        var listOfMahasiswa = arrayOf(
+            Mahasiswa("Wendy Winata", 3.5, 2018),
+            Mahasiswa("Eras Timothy", 3.7, 2018),
+            Mahasiswa("Jonathan", 3.8, 2018),
+            Mahasiswa("Kevin Widjaya", 3.9, 2020),
+            Mahasiswa("Yosia", 3.9, 2018),
+            Mahasiswa("Yotam", 4.0, 2019)
+
+        )
+    }
+}
